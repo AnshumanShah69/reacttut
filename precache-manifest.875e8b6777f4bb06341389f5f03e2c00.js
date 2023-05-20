c763fc3abf4767c1f2ff13859e72cfacfba113f8
ANSHUMAN SHAH <73380314+AnshumanShah69@users.noreply.github.com>
2023-05-20 12:39:41 +0530

    Updates

---
+++ precache-manifest.875e8b6777f4bb06341389f5f03e2c00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cd6698df189622e5b8b",
+    "url": "/reacttut/static/css/main.757f3a40.chunk.css"
+  },
+  {
+    "revision": "8cd6698df189622e5b8b",
+    "url": "/reacttut/static/js/main.b0229f86.chunk.js"
+  },
+  {
+    "revision": "cde8fd35eafe4a33b1d0",
+    "url": "/reacttut/static/js/runtime~main.65add852.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/reacttut/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "cecf616dbfc830b280dc",
+    "url": "/reacttut/static/js/3.d7f7bd74.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/reacttut/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "41f4e7fb409d2f9a13ceaca35edc1945",
+    "url": "/reacttut/index.html"
+  }
+];
